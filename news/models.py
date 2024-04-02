c21f9ea5dded2143d47a25bdc6d163996fffba06
João José <joaojose@Joaos-MacBook-Pro.local>
2024-04-01 21:58:31 -0300

    feature: model User

--- news/models.py
+++ news/models.py
@@ -5,3 +5,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
