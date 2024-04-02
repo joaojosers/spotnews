e0e2b7eb72950372245e9d0081d5972e39a68f55
João José <joaojose@Joaos-MacBook-Pro.local>
2024-04-02 11:47:34 -0300

    debuggin feature model News

--- news/models.py
+++ news/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 from django.core.exceptions import ValidationError
-# from django.forms import ValidationError
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -23,11 +20,21 @@ class User(models.Model):
         return self.name
 
 
+def validate_title(value):
+    if not len(str(value).split()) > 1:
+        raise ValidationError("O título deve conter pelo menos 2 palavras.")
+
+
 class News(models.Model):
     title = models.CharField(
-        max_length=200, validators=[MinLengthValidator(2)]
+        max_length=200,
+        validators=[validate_title],
+        blank=False,
         )
-    content = models.TextField(validators=[MinLengthValidator(1)])
+    content = models.TextField(
+        validators=[MinLengthValidator(1)],
+        blank=False
+        )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateField()
     image = models.ImageField(upload_to='img/', blank=True)
@@ -35,13 +42,13 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
-    
-    def save(self, *args, **kwargs):
-        if self.title and len(self.title.split()) < 2:
-            raise ValidationError(
-                {"title": [_("O título deve conter pelo menos 2 palavras.")]}
-            )
-        super().save(*args, **kwargs)
+
+    # def save(self, *args, **kwargs):
+    #     if self.title and len(self.title.split()) < 2:
+    #         raise ValidationError(
+    #             {"title": [_("O título deve conter pelo menos 2 palavras.")]}
+    #         )
+    #     super().save(*args, **kwargs)
 
     # def save(self, *args, **kwargs):
     #     # teste para novo commit
